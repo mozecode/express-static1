67c15e4c754717a2f0bb16b92b526af03eff4a1f
Greta Moseley <greta.moseley@gmail.com>
2017-10-05 11:46:52 -0500

    express-static exercise 1

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080, ()=>{
+    console.log('listening on port 8080');
+});
